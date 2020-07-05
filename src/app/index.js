893c2c7c87ef4f89312737f8c42d6c35c4eca873
Cesar <rodascesar03@yahoo.com>
2020-07-04 21:44:12 -0500

    added react

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import {Dashboard} from './components/Dashboard';
+import {Main} from './components/Main';
+//console.log(store.getState())
+
+ReactDOM.render(<Main />, document.getElementById('app'));
