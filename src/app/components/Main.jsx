b73d070e6af2833ed212ce3783cd915cf05719c1
Cesar <rodascesar03@yahoo.com>
2020-07-06 22:22:30 -0500

    nav

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Navigation from './Navigation';
+
 export const Main = () => (
   <div>
+    <Navigation />
     <h1>Kaizen</h1>
     <div>Improve your routine.</div>
   </div>
---
+++ src/app/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Navigation = () => (
+  <nav>
+    <a href="#" alt="kaizen logo">Kaizen</a>
+    <ul>
+      <li>Sign in</li>
+      <li>Register</li>
+    </ul>
+  </nav>
+);
