dde9ee5b34c1e31f3c26bd1ed92cd5c0c8982770
Cesar <rodascesar03@yahoo.com>
2020-07-07 22:12:57 -0500

    component test

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-export default Navigation = () => (
-  <nav>
-    <a href="#" alt="kaizen logo">Kaizen</a>
-    <ul>
-      <li>Sign in</li>
-      <li>Register</li>
-    </ul>
-  </nav>
-);
+class Navigation extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#" alt="kaizen logo">Kaizen</a>
+        <ul>
+          <li>Sign in</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
+
